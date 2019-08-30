4908277d2f2e8544e5767fbf954b154639658aef
liujinliang <liujinliang@aspirecn.com>
2019-08-30 16:13:59 +0800

    update ..

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,24 +1,36 @@
 import React from 'react';
 
-class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleChange = this.handleChange.bind(this);
-    this.state = { temperature: '' };
-  }
-  handleChange(e) {
-    this.setState({ temperature: e.target.value });
-  }
-  render() {
-    const temperature = this.state.temperature;
-    return (<fieldset>
-      <legend>Enter temperature in Celsius:</legend>
-      <input value={temperature} onChange={this.handleChange} />
+// class Calculator extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.handleChange = this.handleChange.bind(this);
+//     this.state = { temperature: '' };
+//   }
+//   handleChange(e) {
+//     this.setState({ temperature: e.target.value });
+//   }
+//   render() {
+//     const temperature = this.state.temperature;
+//     return (<fieldset>
+//       <legend>Enter temperature in Celsius:</legend>
+//       <input value={temperature} onChange={this.handleChange} />
 
-      <BoilingVerdict celsius={parseFloat(temperature)} />
-    </fieldset>);
+//       <BoilingVerdict celsius={parseFloat(temperature)} />
+//     </fieldset>);
+//   }
+// }
+
+class Calculator extends React.Component {
+   
+    render() {
+      return (
+          <div>
+              <temperatureInput scale="c" />
+              <temperatureInput scale="f" />
+          </div>
+      );
+    }
   }
-}
 
 function BoilingVerdict(props) {
     if(props.celsius >= 100){
@@ -27,5 +39,34 @@ function BoilingVerdict(props) {
       return <p>The water would not  boil</p>
     }
   }
+
+const scaleNames = {
+     c: 'Celsius',
+     f: 'Fahrenheit'
+};
+
+class temperatureInput extends React.Component {
+    constructor(props){
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+        this.state = {temperature:''};
+    }
+
+    handleChange(e){
+        this.setState({temperature: e.target.value});
+    }
+
+    render (){
+        const temperature  = this.static.temperature;
+        const scale = this.props.scale;
+        return (
+             <fieldset>
+                 <legend>Enter temperature in {scaleNames[scale]}:</legend>
+                 <input value={temperature}
+                        onChange={this.handleChange}/>
+             </fieldset>
+        );
+    }
+}
 
 export default Calculator
